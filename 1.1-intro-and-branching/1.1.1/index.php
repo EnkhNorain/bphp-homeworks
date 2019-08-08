fa06f986dfb03ec6ef1ea49c66bf91f14a05d5d1
EnkhNorain <enoroev@gmail.com>
2019-08-08 12:36:06 +0300

    task fixed

--- 1.1-intro-and-branching/1.1.1/index.php
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -3,39 +3,26 @@ $variable = 3.14;
 //  Ваш программный код, в котором переменной $type
 //  присваивается одно из значений: bool, float, 
 //  int, string, null или other
-if (is_bool($variable)){
-    $type = $variable;
-    echo "$variable is bool";
-} elseif (is_null($variable)) {
-    $type = $variable;
-    echo "$variable is null";
-} elseif (is_string($variable)) {
-    $type = $variable;
-    echo "$variable is string";
+if (is_bool($variable)) {
+    $text = bool;
 } elseif (is_float($variable)) {
-    $type = $variable;
-    echo "$variable is float";
+    $text = float;
 } elseif (is_int($variable)) {
-    $type = $variable;
-    echo "$variable is int";
-} else {
-    $type = $variable;
-    echo "$variable is other";
-}
+    $text = int;
+} elseif (is_string($variable)) {
+    $text = string;
+} elseif (is_null($variable)) {
+    $text = null;
+};
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <title>bPHP - 1.1.1</title>
 </head>
-
 <body>
-    <p>
-        <?=$type?>
-    </p>
+    <p><?=$variable?> is <?=$text?></p>
 </body>
-
 </html>
