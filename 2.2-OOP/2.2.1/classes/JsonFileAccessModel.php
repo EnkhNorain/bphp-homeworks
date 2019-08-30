3b5cf0954200000796f6821fff159ccd317cd728
EnkhNorain <enoroev@gmail.com>
2019-08-30 15:02:46 +0300

    task completed

---
+++ 2.2-OOP/2.2.1/classes/JsonFileAccessModel.php
@@ -0,0 +1,33 @@
+<?php
+    class JsonFileAccessModel extends Config {
+        public function __construct() {
+            $this->fileName = 'files/new.json';
+        }
+        private function connect() {            
+            $this->file = fopen($this->fileName,'r+');
+        }
+        private function disconnect() {
+            fclose($this->file);
+        }
+        public function read() {
+            $this->connect();
+            $text = fread($this->file,3000);
+            $this->disconnect();
+            return $text;            
+        }
+        public function write($text) {
+            if(fopen($this->file,'w+') !== FALSE);
+            if(fwrite($this->file,$text)!== FALSE) echo 'Done';
+            $this->disconnect();
+        }
+        public function readJson() {
+            return json_encode($this->read());
+        }
+        public function writeJson($jsonObject){
+            if(fopen($this->file,'w+') !== FALSE);
+            if(fwrite($this->file,json_decode($jsonObject,JSON_PRETTY_PRINT))!== FALSE) echo 'Done';
+            $this->disconnect();
+        }
+    }
+    
+?>
--- index.php
+++ index.php
@@ -21,6 +21,12 @@
                     <li><a href="1.3-arrays-and-functions/2-search">Задание 2</a></li>
                 </ul>
             </li>
+            <li>Занятие 2.2. Введение в ООП
+                <ul>
+                    <li><a href="2.2-OOP/2.2.1">Задание 1</a></li>
+                    <li><a href="2.2-OOP/2.2.2">Задание 2</a></li>
+                </ul>
+            </li>
         </ul>
     </li>
 </ul>
